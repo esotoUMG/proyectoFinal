6a85e49ebbfa28f85b377712e2d788e26b91dc4a
esotoUMG <tu-correo@example.com>
2025-05-12 20:03:00 -0600

    Agregando plantilla HTML y actualizando la ruta principal en el proyecto Flask

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hola mundo"
+    return render_template("frontend/index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
